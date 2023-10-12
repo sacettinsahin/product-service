9b046fcc1d8b239a3185e46943844f77af3a379d
Sacettin Şahin <sacettin.sahin@etiya.com>
2023-10-12 16:51:22 +0300

    Completed friendlyMessage structure

---
+++ src/main/java/com/xtech/stockmanagement/productservice/exception/enums/FriendlyMessageCodes.java
@@ -0,0 +1,15 @@
+package com.xtech.stockmanagement.productservice.exception.enums;
+
+public enum FriendlyMessageCodes implements IFriendlyMessageCode{
+    OK(1000);
+    private final int value;
+    FriendlyMessageCodes(int value){
+        this.value = value;
+    }
+
+
+    @Override
+    public int getFriendlyMessageCode() {
+        return value;
+    }
+}
